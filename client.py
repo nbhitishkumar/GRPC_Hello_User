0b38d5d067453d65792303c7d6b0aba7bb66765b
Nitish Kumar <50609819+nbhitishkumar@users.noreply.github.com>
2020-02-16 11:25:09 +0530

    Update client.py

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import helloworld_pb2_grpc
 def run():
     with grpc.insecure_channel('127.0.0.1:50051') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='Nitish'))
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='User'))
     print(response.error_messages)
 
 if __name__ == '__main__':
