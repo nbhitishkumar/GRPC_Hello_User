2c8e7ad1135868c8ddf1d10a1a4af428216a4c62
Nitish Kumar <50609819+nbhitishkumar@users.noreply.github.com>
2020-02-16 11:24:46 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import grpc
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel('127.0.0.1:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='Nitish'))
+    print(response.error_messages)
+
+if __name__ == '__main__':
+    run()
